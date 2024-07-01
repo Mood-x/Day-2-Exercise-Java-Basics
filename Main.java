3dbbad94f4f59dc15dbd3da317e7a0d7a4866a23
Mood-x <ag162015@gmail.com>
2024-07-01 18:09:39 +0300

    update

--- Main.java
+++ Main.java
@@ -4,87 +4,87 @@ public class Main {
         Scanner input = new Scanner(System.in);
 // ================ Task 1 ================
 
-//        System.out.print("Enter your weight(kg): ");
-//        float weight = input.nextFloat(); // 70
-//
-//        System.out.print("Enter your height(m): ");
-//        float height = input.nextFloat(); // 1.75
-//
-//        float calcBmi = weight / (height * height);
-//        System.out.printf("BMI = %.2f",calcBmi); // 22.86
+        System.out.print("Enter your weight(kg): ");
+        float weight = input.nextFloat(); // 70
+
+        System.out.print("Enter your height(m): ");
+        float height = input.nextFloat(); // 1.75
+
+        float calcBmi = weight / (height * height);
+        System.out.printf("BMI = %.2f",calcBmi); // 22.86
 
 
 // ================ Task 2 ================
 
-//        System.out.print("Obtained marks = ");
-//        float obMarks = input.nextFloat(); // 85
-//
-//        System.out.print("Total marks = ");
-//        float totalMarks = input.nextFloat(); // 100
-//
-//        float precentage = (obMarks / totalMarks) * 100.0f;
-//        System.out.println("Precentage = "+ precentage + "%"); // 85.0%
+        System.out.print("Obtained marks = ");
+        float obMarks = input.nextFloat(); // 85
+
+        System.out.print("Total marks = ");
+        float totalMarks = input.nextFloat(); // 100
+
+        float precentage = (obMarks / totalMarks) * 100.0f;
+        System.out.println("Precentage = "+ precentage + "%"); // 85.0%
 
 
 // ================ Task 3 ================
 
-//        System.out.print("Amount in USD = ");
-//        float amount = input.nextFloat(); // 100
-//
-//        System.out.print("Exchange Rate (USD to EUR) = ");
-//        float exchangeRate = input.nextFloat(); // 0.85
-//
-//        float converted = amount * exchangeRate;
-//        System.out.println("Amount in EUR = " + converted); // 85.0
+        System.out.print("Amount in USD = ");
+        float amount = input.nextFloat(); // 100
+
+        System.out.print("Exchange Rate (USD to EUR) = ");
+        float exchangeRate = input.nextFloat(); // 0.85
+
+        float converted = amount * exchangeRate;
+        System.out.println("Amount in EUR = " + converted); // 85.0
 
 
 // ================ Task 4 ================
 
-//        System.out.print("Enter the word: ");
-//        String str = input.nextLine(); // Hello, World!
-//        StringBuilder reverse = new StringBuilder(str).reverse();
-//        System.out.println("Length of the string: " + str.length() + " And Reversed string: " + reverse); // 13 , !dlroW ,olleH
+        System.out.print("Enter the word: ");
+        String str = input.nextLine(); // Hello, World!
+        StringBuilder reverse = new StringBuilder(str).reverse();
+        System.out.println("Length of the string: " + str.length() + " And Reversed string: " + reverse); // 13 , !dlroW ,olleH
 
 // ================ Task 5 ================
 
-//        System.out.print("Enter the sentence: ");
-//        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
-//        String subStr = sentence.substring(10, 20);
-//        System.out.println(subStr); // brown fox
+        System.out.print("Enter the sentence: ");
+        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
+        String subStr = sentence.substring(10, 20);
+        System.out.println(subStr); // brown fox
 
 
 // ================ Task 6 ================
 
-//        System.out.print("Enter the sentence: ");
-//        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
-//
-//        System.out.print("Enter the Keyword: ");
-//        String keyword = input.nextLine(); //  jumps
-//
-//        System.out.println("Keyword \"jumps\" is present in the sentence: " + sentence.contains(keyword)); // True
+        System.out.print("Enter the sentence: ");
+        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
+
+        System.out.print("Enter the Keyword: ");
+        String keyword = input.nextLine(); //  jumps
+
+        System.out.println("Keyword \"jumps\" is present in the sentence: " + sentence.contains(keyword)); // True
 
 // ================ Task 7 ================
 
-//        System.out.print("Enter the sentence: ");
-//        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
-//
-//        System.out.print("Enter the replace word: ");
-//        String replaceWord = input.nextLine(); // fox
-//
-//        System.out.print("Enter the replacement word: ");
-//        String replacmentWord = input.nextLine(); // cat
-//
-//        System.out.println(sentence.replaceAll(replaceWord, replacmentWord)); // The quick brown cat jumps over the lazy dog
+        System.out.print("Enter the sentence: ");
+        String sentence = input.nextLine(); // The quick brown fox jumps over the lazy dog
+
+        System.out.print("Enter the replace word: ");
+        String replaceWord = input.nextLine(); // fox
+
+        System.out.print("Enter the replacement word: ");
+        String replacmentWord = input.nextLine(); // cat
+
+        System.out.println(sentence.replaceAll(replaceWord, replacmentWord)); // The quick brown cat jumps over the lazy dog
 
 // ================ Task 8 ================
 
-//        System.out.print("Text one: ");
-//        String textOne = input.nextLine(); // Hello
-//
-//        System.out.print("Text Tow: ");
-//        String textTwo = input.nextLine(); // hello
-//        System.out.println("String are equal (ignoring case): " + textOne.equalsIgnoreCase(textTwo)); // true
-//
+        System.out.print("Text one: ");
+        String textOne = input.nextLine(); // Hello
+
+        System.out.print("Text Tow: ");
+        String textTwo = input.nextLine(); // hello
+        System.out.println("String are equal (ignoring case): " + textOne.equalsIgnoreCase(textTwo)); // true
+
 
     }
 
